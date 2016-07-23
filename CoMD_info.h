7431abc06b1987edf047a02627be83bc1f296eb4
Jeremy Kemp <jakemp@uh.edu>
2016-07-22 18:23:46 -0700

    simplifying make

--- CoMD_info.h
+++ CoMD_info.h
@@ -1,15 +1,15 @@
 #ifndef CoMD_info_hpp
 #define CoMD_info_hpp
 
-#define CoMD_VARIANT "CoMD-openmp"
+#define CoMD_VARIANT ""
 #define CoMD_HOSTNAME "T420X"
 #define CoMD_KERNEL_NAME "'Linux'"
 #define CoMD_KERNEL_RELEASE "'4.4.0-31-generic'"
 #define CoMD_PROCESSOR "'x86_64'"
 
-#define CoMD_COMPILER "'/opt/intel/compilers_and_libraries_2016.3.210/linux/bin/intel64/icc'"
-#define CoMD_COMPILER_VERSION "'icc (ICC) 16.0.3 20160415'"
-#define CoMD_CFLAGS "'-std=c99 -fopenmp -DDOUBLE -g   '"
-#define CoMD_LDFLAGS "'-lm '"
+#define CoMD_COMPILER "''"
+#define CoMD_COMPILER_VERSION "'unknown'"
+#define CoMD_CFLAGS "''"
+#define CoMD_LDFLAGS "''"
 
 #endif
